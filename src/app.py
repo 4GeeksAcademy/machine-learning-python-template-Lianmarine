f4309bc2b9d5286629f99d289a1f262d0bc90486
Agustin Henry Marine Monteiro <141757322+lianmarine@users.noreply.github.com>
2023-10-20 14:15:20 +0000

    Day 1

--- src/app.py
+++ src/app.py
@@ -1,4 +1,19 @@
-from utils import db_connect
-engine = db_connect()
+import requests
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.impute import KNNImputer
+import statsmodels.api as sm
 
-# your code here
+dataset = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+df = pd.DataFrame(dataset)
+
+print(df.info())
+count_unique = df.nunique()
+df.describe()
+
+y = df.price.values
+print(y)
+plt.hist(y, bins=20)
+plt.show
